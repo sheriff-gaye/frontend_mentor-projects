8d83350072fe35504856cbbf8c9a4ad0bf2c0ca1
sheriff-gaye <sheriffgaye5@gmail.com>
2023-04-06 20:22:49 +0000

    all

---
+++ tip-calculator-app-main/index.js
@@ -0,0 +1,21 @@
+//javascript work
+
+const bill_amount =document.getElementById('bill');
+const perosn_num=document.getElementById('num');
+const button=document.querySelectorAll('.btn');
+
+
+const tip_amount=document.getElementById('tip_amount');
+const tip_total=document.getElementById('tip_total')
+
+
+
+
+
+
+
+function calculate(){
+    console.log(bill.value,num.value);
+
+
+}
