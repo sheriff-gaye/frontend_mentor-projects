ac6159af7c3f9d0402cc0dcb559995d1a3a9ec79
sheriff-gaye <sheriffgaye5@gmail.com>
2023-04-08 16:51:29 +0000

    all

--- tip-calculator-app-main/index.js
+++ tip-calculator-app-main/index.js
@@ -6,34 +6,36 @@ const person_num = document.getElementById('num');
 const buttons = document.querySelectorAll('.btn');
 const custom = document.getElementById('custom')
 
-
 const tip_amount = document.getElementById('tip_amount');
 const tip_total = document.getElementById('tip_total');
 let rate = 0;
 
-
-
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         rate = (e.target.value) / 100;
+        button.classList.remove('active');
+
         calculate();
     })
 
-})
 
+})
 
 function calculate() {
 
-    if (billValue > 0 && numValue > 0 && rate > 0) {
+    if ((billValue && numValue && rate) > 0) {
         tip = (billValue * rate);
-        tip_per_person = tip / 2;
         tip_plus = billValue + tip;
         result_person = (tip_plus / numValue);
+
+        if (numValue > 1) {
+            tip_per_person = tip / 2;
+        }
+        tip_per_person = tip;
 
 
         tip_amount.innerHTML = `$` + (tip_per_person.toFixed(2));
         tip_total.innerHTML = `$` + (result_person.toFixed(2));
-
 
     }
     else {
@@ -42,35 +44,37 @@ function calculate() {
 
     }
 
+
 }
 
 bill_amount.addEventListener('input', (e) => {
-    bill = e.target.value.trim();
-    billValue = Number(bill);
+    billValue = Number(e.target.value);
     calculate();
-
 })
 
 person_num.addEventListener('input', (e) => {
-    num = e.target.value.trim();
-    numValue = Number(num)
+    numValue = Number(e.target.value);
     calculate();
 })
 
-
 custom.addEventListener('input', (e) => {
-    cus = e.target.value.trim();
+    cus = e.target.value;
     rate = Number(cus) / 100;
     calculate();
 })
 
 btn.addEventListener('click', () => {
 
-    bill_amount.value = "";
+    billValue.value = 0;
+    bill_amount.value = ""
+
     person_num.value = "";
-    rate = "";
+    numValue = 0;
+
+    custom.value = ""
+    rate = 0;
+
     tip_amount.innerHTML = `$` + (0.00.toFixed(2));
     tip_total.innerHTML = `$` + (0.00.toFixed(2));
-
 
 })
